83a6af3eb5d769c7fcaa4ab30edec60925b8d078
ahmadmeir <a.meirghadam@tilburguniversity.edu>
2026-02-20 15:10:51 +0100

    Removed the isAdult and endYear variables and movies with genre = NA

--- src/data-preparation/data-cleaning.R
+++ src/data-preparation/data-cleaning.R
@@ -8,22 +8,27 @@ library(tidyverse)
 
 # Read data
 
-title.basics <-  read_tsv("data/raw/title.basics.tsv.gz")
-title.ratings <- read_tsv("data/raw/title.ratings.tsv.gz")
+title.basics  <- read_tsv("data/raw/title.basics.tsv.gz", na = "\\N")
+title.ratings <- read_tsv("data/raw/title.ratings.tsv.gz", na = "\\N")
 
 # Overview of titleTypes
+# 
+# title.basics %>%
+#   group_by(titleType) %>%
+#   summarise(count = n()) %>%
+#   arrange(desc(count))
 
-title.basics %>%
-  group_by(titleType) %>%
-  summarise(count = n()) %>%
-  arrange(desc(count))
-
-# Select Movies Only
+# Select movies only
 title.basics_movies <- filter(title.basics, titleType == "movie")
 
 # Merging the title.basics and title.ratings datasets
 
 IMDB_movies <- left_join(title.basics_movies, title.ratings, by = "tconst")
+
+# Remove movies without genre and drop isAdult and endYear
+IMDB_movies <- IMDB_movies %>%
+  filter(!is.na(genres)) %>%
+  select(-isAdult, -endYear)
 
 # Splitting genres
 IMDB_movies <- IMDB_movies %>% 
@@ -34,10 +39,5 @@ IMDB_movies <- IMDB_movies %>%
               values_fill = 0,
               names_prefix = "genre_")
 
-# List of genres
-
-names(IMDB_movies)[grepl("genre", names(IMDB_movies))]
-
-# Are the variables isAdult and genre_Adult identical?
-identical(IMDB_movies$isAdult, IMDB_movies$genre_Adult)
-
+#  List of genres
+# names(IMDB_movies)[grepl("genre", names(IMDB_movies))]
